00cab4e5cac5d29aceb1ca3b6b4b2987ea82893d
CharlesMrtZ <charles.martinazzo@gmail.com>
2026-05-02 17:58:26 -0300

    add item de UI, expansão de zonas

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 let personagem;
 let tecla;
+let teclaE;
+
+let podeInteragir;
+let textoInteracao;
+
+//zona hardcoded (teste temporário)
+let zonaCasa1;
+
 const velocidade = 100;
 
 const config = {
@@ -52,6 +60,8 @@ function create() {
     const terrain2Layer = map.createLayer('terrain2', tilesetIsland, 0, 0);
     const objectsLayer = map.createLayer('objects', [tilesetObjects2, tilesetFence], 0, 0);
     const objects2Layer = map.createLayer('objects2', tilesetObjects2, 0, 0);
+
+    //colisão
     waterLayer.setCollisionByProperty({ collider: true });
     terrain2Layer.setCollisionByProperty({ collider: true });
 
@@ -59,37 +69,90 @@ function create() {
 
     //personagem
     personagem = this.physics.add.sprite(180, 200, 'personagem');
+    personagem.body.setSize(32, 32);
+    personagem.body.setOffset(0, 38);
+
+    //teclas
     tecla = this.input.keyboard.addKeys({
         up: 'W',
         left: 'A',
         down: 'S',
         right: 'D',
-        shift: Phaser.Input.Keyboard.KeyCodes.SHIFT
+        shift: Phaser.Input.Keyboard.KeyCodes.SHIFT,
     });
-    personagem.body.setSize(32, 32);
-    personagem.body.setOffset(0, 38);
+
+    teclaE = this.input.keyboard.addKey('E');
+
+    //colisores
     this.physics.add.collider(personagem, waterLayer);
     this.physics.add.collider(personagem, terrain2Layer)
 
-    //camera
+    //zonas
+    zonaCasa1 = this.add.zone(400, 170, 250, 140);
+    this.physics.add.existing(zonaCasa1);
+    zonaCasa1.body.setAllowGravity(false);
+    zonaCasa1.body.setImmovable(true);
+    zonaCasa1.url = "https://www.twitch.tv/sovousetuvai";
+
+    zonaCasa2 = this.add.zone(470, 460, 250, 140);
+    this.physics.add.existing(zonaCasa2);
+    zonaCasa2.body.setAllowGravity(false);
+    zonaCasa2.body.setImmovable(true);
+    zonaCasa2.url = "https://www.youtube.com/@sovousetuvai";
+
+    //cameras
+    //mundo
     this.cameras.main.startFollow(personagem);
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.setZoom(3);
+    this.cameras.main.setZoom(1);
+
+    //UI
+    const uiCamera = this.cameras.add(0, 0, 1280, 900);
+    uiCamera.setScroll(0, 0)
+    uiCamera.ignore([waterLayer, terrainLayer, terrain2Layer, objectsLayer, objects2Layer, personagem])
+
 
     //interações de UI
-    textoInteracao = this.add.text(0, 0, 'Pressione E', {
-        fontSize: '16px',
+    textoInteracao = this.add.text(640, 850, 'Pressione E', {
+        fontSize: '36px',
         fill: '#ffffff',
         backgroundColor: '#000000'
     });
-    textoInteracao.setVisible(true);
-    textoInteracao.setScrollFactor(0);
-    textoInteracao.setPosition(50, 80);
+    //textoInteracao.setVisible(false);
+    textoInteracao.setOrigin(0.5);
+    textoInteracao.setDepth(1000);
+    this.cameras.main.ignore([textoInteracao])
+
+    podeInteragir = false;
+
+    this.physics.add.overlap(personagem, zonaCasa1, () => {
+        podeInteragir = true;
+    });
 
 }
 
 function update() {
     personagem.setVelocity(0);
+
+
+    podeInteragir = false;
+
+    let zonaAtual = null;
+    if (this.physics.overlap(personagem, zonaCasa1)) {
+        zonaAtual = zonaCasa1;
+    } else {
+        zonaAtual = zonaCasa2
+    }
+
+    if (this.physics.overlap(personagem, zonaAtual)) {
+        podeInteragir = true;
+    };
+
+    if (podeInteragir) {
+        textoInteracao.setVisible(true);
+    } else {
+        textoInteracao.setVisible(false);
+    }
 
     let velocidadeAtual = velocidade;
 
@@ -100,4 +163,8 @@ function update() {
     if (tecla.up.isDown) { personagem.setVelocityY(-velocidadeAtual) };
     if (tecla.down.isDown) { personagem.setVelocityY(+velocidadeAtual) };
 
+
+    if (podeInteragir && Phaser.Input.Keyboard.JustDown(teclaE)) {
+        window.open(zonaAtual.url, '_blank')
+    }
 }
